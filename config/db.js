0c56c59e3099e5fd8c5fe0f84764932664744b7e
Kajal8791 <km6522391@gmail.com>
2025-06-04 21:30:34 +0530

    Fix: Removed undefined condition and successful build

--- config/db.js
+++ config/db.js
@@ -1,31 +1,26 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
-
-if (!condition) {
-     cached = global.mongoose = { conn: null, promise: null };
-}
+// Agar global.mongoose defined nahi hai, to usko initialize karo
+let cached = global.mongoose || { conn: null, promise: null };
+global.mongoose = cached;
 
 async function connectDB() {
-
-    if (cached.conn) {
-        return cached.conn;
+  if (cached.conn) {
+    return cached.conn;
   }
 
   if (!cached.promise) {
-      const opts = {
-        bufferCommands: false,
+    const opts = {
+      bufferCommands: false,
     };
 
-        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts).then((mongoose) => {
-        return mongoose;
-      });
-      
+    cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts).then((mongoose) => {
+      return mongoose;
+    });
   }
 
   cached.conn = await cached.promise;
   return cached.conn;
-
 }
 
-export default connectDB
+export default connectDB;
